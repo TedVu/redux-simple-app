8f2d6cc5fee3ebef9fc3c64ab3c76ddd7c68ca3d
tedvu <tedvu184@gmail.com>
2022-03-06 16:23:42 +1100

    Use margin as spacing technique

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,25 +3,21 @@ import { connect } from "react-redux";
 
 class SongList extends React.Component {
     renderList() {
-        return (
-            <div className="grid gap-y-6">
-                {this.props.songs.map((song) => {
-                    return (
-                        <div className="grid grid-cols-3" key={song.title}>
-                            <div className="col-start-1 col-end-2 text-center">
-                                {song.title}
-                            </div>
-                            <div className="col-start-2 col-end-3 text-center">
-                                {" "}
-                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                    Select
-                                </button>
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
-        );
+        return this.props.songs.map((song) => {
+            return (
+                <div className="grid grid-cols-3 mt-20" key={song.title}>
+                    <div className="col-start-1 col-end-2 text-center">
+                        {song.title}
+                    </div>
+                    <div className="col-start-2 col-end-3 text-center">
+                        {" "}
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Select
+                        </button>
+                    </div>
+                </div>
+            );
+        });
     }
 
     render() {
